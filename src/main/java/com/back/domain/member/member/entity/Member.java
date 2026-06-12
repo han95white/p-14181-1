6b50ef7e5f816f680648def3f189fea4d07addac
DoubleSang <hdsa1564@gmail.com>
2026-06-12 12:18:09 +0900

    006

--- src/main/java/com/back/domain/member/member/entity/Member.java
+++ src/main/java/com/back/domain/member/member/entity/Member.java
@@ -18,4 +18,8 @@ public class Member extends BaseEntity {
         this.password = password;
         this.nickname = nickname;
     }
+
+    public String getNickname() {
+        return nickname;
+    }
 }
