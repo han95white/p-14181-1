0ee30e482ad4b62b8db2301ff56e6e010c6eff4a
DoubleSang <hdsa1564@gmail.com>
2026-06-16 10:21:54 +0900

    037

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -70,8 +70,7 @@ public class ApiV1PostCommentController {
         Post post = postService.findById(postId).get();
         PostComment postComment = post.findCommentById(id).get();
 
-        if (!actor.equals(postComment.getAuthor()))
-            throw new ServiceException("403-1", "댓글 삭제 권한이 없습니다.");
+        postComment.checkActorCanDelete(actor);
 
         postService.deleteComment(post, postComment);
 
